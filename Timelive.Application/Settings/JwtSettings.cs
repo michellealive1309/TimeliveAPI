0c35810022d2c2e3e0ccd0ec8e910f613802ce15
Pitiphat <michellealive1309@gmail.com>
2025-03-09 16:23:25 +0700

    Add jwt settings

---
+++ Timelive.Application/Settings/JwtSettings.cs
@@ -0,0 +1,8 @@
+namespace Timelive.Application.Settings;
+
+public class JwtSettings
+{
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+}
