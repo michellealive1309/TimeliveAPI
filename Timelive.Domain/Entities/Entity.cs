2486342add157607992be37b378abf7f289620e6
Pitiphat <michellealive1309@gmail.com>
2025-03-09 16:19:03 +0700

    Add Entity and User entity

---
+++ Timelive.Domain/Entities/Entity.cs
@@ -0,0 +1,9 @@
+namespace Timelive.Domain.Entities;
+
+public abstract class Entity
+{
+    public int Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
