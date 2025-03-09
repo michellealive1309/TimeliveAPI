5419ab5f47e0b50d123a651aeff35f4ff02d14d2
Pitiphat <michellealive1309@gmail.com>
2025-03-09 16:25:03 +0700

    Add api configuration

---
+++ Timelive.API/Program.cs
@@ -0,0 +1,55 @@
+using System.Text.Json;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Timelive.API.Extensions;
+using Timelive.Application.Extensions;
+using Timelive.Infrastructure.DataAccess;
+using Timelive.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services
+    .AddHealthChecks()
+    .AddDbContextCheck<ApplicationDbContext>();
+
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddHealthChecks();
+}
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"), b => b.MigrationsAssembly("Timelive.Infrastructure"));
+});
+
+builder.Services.AddSwagger();
+builder.Services.AddAuth(builder.Configuration);
+builder.Services.AddProvider();
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapHealthChecks("/api/health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapControllers();
+
+app.Run();
