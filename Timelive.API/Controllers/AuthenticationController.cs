6f1d69f21db09b5848f21a5af97d0548af6f3e21
Pitiphat <michellealive1309@gmail.com>
2025-03-09 16:23:37 +0700

    Add authentication controller

---
+++ Timelive.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Timelive.Application.DTOs.Authentication;
+using Timelive.Application.Interfaces;
+
+namespace Timelive.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> Signin([FromBody] LoginDto loginDto)
+        {
+            var accessToken = await _authenticationService.LoginAsync(loginDto);
+            if (accessToken == "")
+            {
+                return Unauthorized();
+            }
+
+            var refreshToken = await _authenticationService.UpdateRefreshTokenAsync(loginDto.Email);
+            Response.Cookies.Append("refresh_token", refreshToken, new CookieOptions {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddDays(30)
+            });
+
+            return Ok(accessToken);
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Signup([FromBody] RegisterDto registerDto)
+        {
+            var userResponse = await _authenticationService.CreateUserAsync(registerDto);
+            return Ok(userResponse);
+        }
+    }
+}
