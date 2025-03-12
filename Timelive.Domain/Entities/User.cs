c603589467b683f0224f5a55adfd236ba651ef7f
Pitiphat <michellealive1309@gmail.com>
2025-03-12 12:46:53 +0700

    Refactor User entity to enforce non-nullable properties and change Role type to UserRole

--- Timelive.Domain/Entities/User.cs
+++ Timelive.Domain/Entities/User.cs
@@ -1,10 +1,12 @@
+using Timelive.Domain.Enums;
+
 namespace Timelive.Domain.Entities;
 
 public class User : Entity
 {
-    public string? Email { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
-    public string? Role { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public UserRole Role { get; set; }
     public string? Token { get; set; }
 }
