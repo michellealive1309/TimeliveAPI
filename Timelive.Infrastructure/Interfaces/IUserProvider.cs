a233686942324046241065436545a3b2b5417da2
Pitiphat <michellealive1309@gmail.com>
2025-03-12 12:55:55 +0700

    Add IUserProvider interface and update UserProvider to include user role retrieval

--- Timelive.Infrastructure/Interfaces/IUserProvider.cs
+++ Timelive.Infrastructure/Interfaces/IUserProvider.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace Timelive.Infrastructure.Providers;
+namespace Timelive.Infrastructure.Interfaces;
 
 public interface IUserProvider
 {
--- Timelive.Infrastructure/Providers/UserProvider.cs
+++ Timelive.Infrastructure/Providers/UserProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using Timelive.Infrastructure.Interfaces;
 
 namespace Timelive.Infrastructure.Providers;
 
