9c566a0fdc37c43c4d0f3ec5340c5ca42da5d38f
Pitiphat <michellealive1309@gmail.com>
2025-03-15 11:39:26 +0700

    Register repository to ServiceCollection

--- Timelive.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Timelive.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Timelive.Domain.Interfaces;
+using Timelive.Infrastructure.Interfaces;
 using Timelive.Infrastructure.Providers;
 using Timelive.Infrastructure.Repositories;
 
@@ -9,6 +10,14 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
+        services.AddScoped<IGroupRepository, GroupRepository>();
+        services.AddScoped<IImageRepository, ImageRepository>();
+        services.AddScoped<ILikeRepository, LikeRepository>();
+        services.AddScoped<IProfileRepository, ProfileRepository>();
+        services.AddScoped<IPurchasedStoryRepository, PurchasedStoryRepository>();
+        services.AddScoped<IStoryRepository, StoryRepository>();
+        services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+        services.AddScoped<ITopicRepository, TopicRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
 
         return services;
@@ -17,6 +26,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddProvider(this IServiceCollection services)
     {
         services.AddScoped<IUserProvider, UserProvider>();
+        services.AddScoped(typeof(ICacheProvider<>), typeof(RedisCacheProvider<>));
 
         return services;
     }
