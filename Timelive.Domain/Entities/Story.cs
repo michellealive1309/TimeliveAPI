a7112bc9c73bf677bf6c2215c437843fe9b1def3
Pitiphat <michellealive1309@gmail.com>
2025-03-15 11:40:52 +0700

    Add IsPublished flag and relationships for Likes and PurchasedStories in Story entity

--- Timelive.Domain/Entities/Story.cs
+++ Timelive.Domain/Entities/Story.cs
@@ -10,10 +10,13 @@ public class Story : Entity
     public ContentType ContentType { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public bool IsSubscribeNeed { get; set; } = false;
+    public bool IsPublished { get; set; } = false;
     public bool IsPurchaseNeed { get; set; } = false;
     public int TopicId { get; set; }
     public int? WriterGroupId { get; set; }
     public int WriterId { get; set; }
+    public IEnumerable<Like>? Likes { get; set; }
+    public IEnumerable<PurchasedStory>? PurchasedStories { get; set; }
     public Group? WriterGroup { get; set; }
     public Story? ParentStory { get; set; }
     public Profile? Writer { get; set; }
